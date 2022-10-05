29b519b7b5f7b281835b32afaae8ff66b85d13c1
Flavio Troia <flavio.troia@gmail.com>
2022-10-05 18:15:20 +0200

    (feat): Added city style

--- pages/[...slug]/index.tsx
+++ pages/[...slug]/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Layout from "../../layouts/Layout";
 import HeaderPage from "../../components/header/Header";
 import { PrismaClient, City } from "@prisma/client";
+import React from "react";
 
 type CityProps = {
     city: City;
@@ -11,32 +12,61 @@ const CityPage: NextPage<CityProps> = ({ city }) => {
     return (
         <Layout>
             <HeaderPage />
-            <h1>{city.name}</h1>
+            <div id="cities" className="cards-1">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <h2 className="heading">
+                                UfficioAffissioni ha tutte le caratteristiche di cui <span>hai bisogno</span>
+                            </h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div>
+                                <div className="card-icon">
+                                <span>{city.province}</span>
+                            </div>
+                            <div className="card-body">
+                                <h4 className="card-title">{city.name}</h4>
+                                <p>{city.geography}</p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
-    const prisma = new PrismaClient();
-    console.log(context.params);
+    try {
+        const prisma = new PrismaClient();
+        const city = await prisma.city.findFirst({
+            where: {
+                slug: context.params?.slug?.[0] ?? ""
+            }
+        });
 
-    const city = await prisma.city.findFirst({
-        where: {
-            slug: context.params?.slug?.[0] ?? ""
+        if (!city) {
+            return {
+                notFound: true
+            }
         }
-    });
-    if (!city) {
+
+        return {
+            props: {
+                city
+            } as CityProps
+        }
+    } catch (err) {
         return {
             notFound: true
         }
     }
-
-    return {
-        props: {
-            city
-        } as CityProps
-    }
 }
+
 
 export const getStaticPaths: GetStaticPaths = () => {
     return {
