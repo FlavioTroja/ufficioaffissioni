2582ee691086b21e8f182101aa9e743bb4aef166
Flavio Troia <flavio.troia@gmail.com>
2022-12-05 10:53:40 +0100

    (feat): Fixed import bootstrap css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import '../styles/theme.scss'
-import type { AppProps } from 'next/app'
+import "../styles/theme.scss"
+import type { AppProps } from "next/app"
 import {useEffect} from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
+    console.log("DOC: ", document);
     typeof document !== undefined ? require("bootstrap/dist/js/bootstrap") : null
   }, []);
 
