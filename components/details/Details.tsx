bdebf1f9bd20e6d5b5102d336377668d27da5227
Flavio Troia <flavio.troia@gmail.com>
2022-12-13 13:06:29 +0100

    (feat): Fixed features scss

--- components/details/Details.tsx
+++ components/details/Details.tsx
@@ -7,7 +7,7 @@ const Details: React.FC = () => {
                         <div className="col-lg-6 col-xl-5">
                             <div className="text-container">
                                 <h2>Consulente Pubblicitario</h2>
-                                <p>Ti proponiamo un consulente dedicato e specializzato nel tuo business e pronto a rispondere alle tue domande.</p>
+                                <p>Ti proponiamo un consulente dedicato e specializzato nel tuo business e pronto a rispondere alle tue domande</p>
                                 <a href="#pricing">Prezzi</a>
                             </div>
                         </div>
